6b438a7ef6d6fc6e8971f98534b4a9df4812b014
murad <muradkv@gmail.com>
2025-03-05 20:59:03 +0300

    Initial commit

---
+++ Bakery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bakery
+//
+//  Created by murad on 05.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+
+
+}
+
